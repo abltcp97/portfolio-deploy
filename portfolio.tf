9ed0253ce917fb3907861faa83e470e3cdeade32
abltcp97 <abltcp97@gmail.com>
2025-04-07 14:14:34 -0700

    Fix: have terraform wait until block settings updated before applying update.

--- portfolio.tf
+++ portfolio.tf
@@ -37,11 +37,21 @@ resource "aws_s3_bucket_policy" "allow_public_read" {
     Version = "2012-10-17",
     Statement = [
       {
-        Effect    = "Allow",
-        Principal = "*",
-        Action    = "s3:GetObject",
-        Resource  = "arn:aws:s3:::portfolio-site-aalamillo/*"
+        Sid = "AllowCloudFrontServicePrincipalReadOnly"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.portfolio_site.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.portfolio.arn
+          }
+        }
       }
     ]
   })
+
+  depends_on = [aws_s3_bucket_public_access_block.portfolio_site]
 }
